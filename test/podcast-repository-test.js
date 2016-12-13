45c5107d71edfcd758b3edfd2cc68abd13b2e4a7
alessiocoser <alessio.coser@xpeppers.com>
2016-12-13 14:41:30 +0100

    fix s3Database issues

--- test/podcast-repository-test.js
+++ test/podcast-repository-test.js
@@ -2,16 +2,33 @@ const assert = require('assert')
 const PodcastRepository = require('../lib/podcast-repository')
 
 describe('PodcastRepository', function () {
+  this.timeout(10000)
+
   it('returns empty array if name not found', function () {
     var podcasts = new PodcastRepository()
-
-    assert.deepEqual(podcasts.search('notfound'), [])
+    return podcasts.search({category: 'notfound'})
+    .then((pods) => {
+      assert.deepEqual(pods, [])
+    })
   })
 
   it('returns a simple item', function () {
-    var expectedPayload = {"title":"Burro d'Arachidi - 06x09","file":{"url":"http://www.sanbaradio.it/files/barachidi_06x09.mp3","length":"28750595","type":"audio/mpeg"}}
+    var expectedPayload = {
+      "category": "Burro d'Arachidi",
+      "date": "2016-12-04T18:08:23.000Z",
+      "description": "",
+      "episode": "06x09",
+       "file": {
+         "length": "28750595",
+         "type": "audio/mpeg",
+         "url": "http://www.sanbaradio.it/files/barachidi_06x09.mp3"
+       }
+    }
     var podcasts = new PodcastRepository('sanbaradio-podcasts', 'pods.json')
 
-    assert.deepEqual(podcasts.find("Burro d'Arachidi - 06x09"), [expectedPayload])
+    return podcasts.search({category: "Burro d'Arachidi", episode: '06x09'})
+    .then((pods) => {
+      assert.deepEqual(pods, [expectedPayload])
+    })
   })
 })
