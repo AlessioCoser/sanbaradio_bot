306b1291ecf3707c484b95435f28acf31202fe2f
alessiocoser <alessio.coser@xpeppers.com>
2016-12-20 15:11:07 +0100

    returns full payload stringified

--- bot.js
+++ bot.js
@@ -2,5 +2,5 @@ var botBuilder = require('claudia-bot-builder')
 
 module.exports = botBuilder(function (request) {
 
-  return 'Hi!'
+  return 'Ciao grazie per avermi scritto questo: ' + JSON.stringify(request)
 })
